2a3bd288e9f1e5cbd25eca82d86ce05f629f8356
cheolmin-s <scmjazz@naver.com>
2019-04-11 12:24:04 +0900

    no message

--- js_src/antenna_2-1.js
+++ js_src/antenna_2-1.js
@@ -4,17 +4,17 @@ $(function(){
 
     $('.contents-list-item').on('click',function(){
 
-        $('body').css({overflow:'hidden'});
+        $('html,body').css({overflow:'hidden'}).bind('touchmove');
 
         $(this).children($popup).fadeIn();
 
     });
 
-    $('.popupBg').on('click', function(e){
+    $('.popupBg, .popup-wrap-close').on('click', function(e){
 
         e.stopPropagation();
 
-        $('body').css({overflow:'visible'});
+        $('html,body').css({overflow:'visible'}).unbind('touchmove');
 
         $popup.fadeOut();
     });
